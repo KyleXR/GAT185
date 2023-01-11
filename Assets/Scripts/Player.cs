bc6e6d2ca7f8749b802de53a33b99349705747a8
KyleXR <98604126+KyleXR@users.noreply.github.com>
2023-01-11 12:37:57 -0700

    Class 1/11/2023

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Range(1, 10), Tooltip("Controls the speed of the player")] public float speed = 5;
+    [Range(1, 60), Tooltip("Controls the speed of the player")] public float speed = 10;
+    [Range(1, 360)] public float rotationRate = 180;
     public GameObject prefab;
+    public Transform bulletSpawn;
 
     private void Awake()
     {
@@ -25,23 +27,21 @@ public class Player : MonoBehaviour
         //transform.localScale = Vector3.one * Random.value * 5;
 
         Vector3 direction = Vector3.zero; // Vecter{0,0,0}
-
-        direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
 
-        //if (Input.GetKey(KeyCode.A)) direction.x = -1;
-        //if (Input.GetKey(KeyCode.D)) direction.x = +1;
-        //if (Input.GetKey(KeyCode.W)) direction.z = +1;
-        //if (Input.GetKey(KeyCode.S)) direction.z = -1;
+        Vector3 rotation = Vector3.zero;
+        rotation.y = Input.GetAxis("Horizontal");
 
-
-        transform.position += direction * speed * Time.deltaTime;
+        Quaternion rotate = Quaternion.Euler(rotation * rotationRate * Time.deltaTime);
+        transform.rotation = transform.rotation * rotate;
+        transform.Translate(direction * speed * Time.deltaTime);
+        //transform.position += direction * speed * Time.deltaTime;
 
         if(Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("pew!");
-            GetComponent<AudioSource>().Play();
-            Instantiate(prefab, transform.position, transform.rotation);
+            //Debug.Log("pew!");
+            //GetComponent<AudioSource>().Play();
+            GameObject go = Instantiate(prefab, bulletSpawn.position, bulletSpawn.rotation);
         }
     }
 }
