5668a663d8cdbb955ada61842aa7d697f94f1774
KyleXR <98604126+KyleXR@users.noreply.github.com>
2023-01-12 11:53:48 -0700

    Class 1/12/2023

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,12 +8,19 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) return;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
     }
 }
